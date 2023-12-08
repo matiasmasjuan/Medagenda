be7290ff1fc44b9dda3b51d54d70db52999270e0
matiasmasjuan <matas.masjuan@uc.cl>
2023-12-08 18:32:31 -0300

    :sparkles: Add User Endpoints

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
+const userRoutes = require('./routes/user');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-// app.use('/api/users', require('./routes/api/users'));
+app.use('/api/users', userRoutes);
 // app.use('/api/ranking', require('./routes/api/ranking'));
 
 app.get('/', (_, res) => {
