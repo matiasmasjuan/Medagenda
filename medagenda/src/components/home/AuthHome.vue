d1a32c24fa2929614955cdd3546fa53a887cd916
matiasmasjuan <matas.masjuan@uc.cl>
2023-12-14 14:56:47 -0300

    :art: Change Home Style depending on user role

--- medagenda/src/components/home/AuthHome.vue
+++ medagenda/src/components/home/AuthHome.vue
@@ -6,23 +6,37 @@
         <h1 class="text-5xl mt-16">
           Hola <span class="font-bold text-primary">{{ user.name }}</span> 👋
         </h1>
-        <p class="text-2xl text-gray-500 py-8">¿Necesitas agendar una consulta?</p>
+        <div v-if="user.role === 'Patient'">
+          <p class="text-2xl text-gray-500 py-8">¿Necesitas agendar una consulta?</p>
 
-        <div class="flex justify-left ml-20">
-          <RouterLink :to="{ name: 'scheduler' }">
-            <button class="text-left px-8 py-2 bg-primary text-white hover:text-lgray rounded-md">
-              Agendar Hora
-            </button>
-          </RouterLink>
+          <div class="flex justify-left ml-20">
+            <RouterLink :to="{ name: 'scheduler' }">
+              <button class="text-left px-8 py-2 bg-primary text-white hover:text-lgray rounded-md">
+                Agendar Hora
+              </button>
+            </RouterLink>
+          </div>
+
+          <div class="flex justify-left">
+            <p class="text-gray-600 pt-4">
+              ¿Ya tienes una hora reservada?
+              <RouterLink :to="{ name: 'my-schedule' }">
+                <span class="text-primary hover:text-primary80">Revisa tu agenda</span>
+              </RouterLink>
+            </p>
+          </div>
         </div>
 
-        <div class="flex justify-left">
-          <p class="text-gray-600 pt-4">
-            ¿Ya tienes una hora reservada?
+        <div v-else>
+          <p class="text-2xl text-gray-500 py-8">Que bueno tenerte de vuelta!</p>
+
+          <div class="flex justify-left ml-20">
             <RouterLink :to="{ name: 'my-schedule' }">
-              <span class="text-primary hover:text-primary80">Revisa tu agenda</span>
+              <button class="text-left px-8 py-2 bg-primary text-white hover:text-lgray rounded-md">
+                Ver mi Agenda
+              </button>
             </RouterLink>
-          </p>
+          </div>
         </div>
 
       </div>
